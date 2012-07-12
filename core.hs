0693b4e0a7c9f7875601faf122da88a778fe3d51
Yngve Devik Hammersland <yngve@me.com>
2012-07-12 22:58:57 +0200

    Added initial implementation. It simply displays the title and waits for a keypress before exiting.

--- core.hs
+++ core.hs
@@ -1,1 +1,12 @@
-main = print "Hello"
+import UI.HSCurses.Curses as C
+import UI.HSCurses.CursesHelper as H
+
+main = do
+	H.start
+	(sizeY, sizeX) <- C.scrSize
+	C.mvWAddStr C.stdScr (sizeY `div` 2 - 1) (sizeX `div` 2 - 13) "Welcome to Caves of Haskell"
+	C.mvWAddStr C.stdScr (sizeY `div` 2) (sizeX `div` 2 - 13)     ":-----------=o=-----------:"
+	C.mvWAddStr C.stdScr (sizeY `div` 2 + 3) (sizeX `div` 2 - 12) "Press any key to exit..."
+	C.refresh
+	c <- C.getCh
+	H.end
