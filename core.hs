c3f7351d34d476caf67fcc88792a8bbe0f17b965
Yngve Devik Hammersland <yngve@me.com>
2012-07-15 01:43:02 +0200

    Added map smoothing.

--- core.hs
+++ core.hs
@@ -4,10 +4,11 @@ import UI.HSCurses.Curses as C
 import UI.HSCurses.CursesHelper as H
 import Data.Time.Clock (getCurrentTime)
 
+data Pos = Pos Int Int
 data Size = Size Int Int deriving (Show, Eq)
 data Screen = Start | Play | Win | Lose | Quit deriving (Show)
-data TileKind = Wall | Floor | Bound deriving (Show)
-data Tile = Tile { kind :: TileKind, glyph :: Char, color :: H.ForegroundColor } deriving (Show)
+data TileKind = Wall | Floor | Bound deriving (Show, Eq)
+data Tile = Tile { kind :: TileKind, glyph :: Char, color :: H.ForegroundColor } deriving (Show, Eq)
 
 wall  = Tile Wall  '#' H.WhiteF
 floor = Tile Floor '.' H.WhiteF
@@ -41,7 +42,29 @@ newMap size g = newMap' size g where
 		(row , g' ) = newRow dx g
 		(rows, g'') = newMap' (Size dx (dy-1)) g'
 
-getTile (World size tiles) x y = tiles !! y !! x
+dirN (Pos x y) = Pos x (y+1)
+dirS (Pos x y) = Pos x (y-1)
+dirW (Pos x y) = Pos (x-1) y
+dirE (Pos x y) = Pos (x+1) y
+dirNE = dirN . dirE
+dirNW = dirN . dirW
+dirSE = dirS . dirE
+dirSW = dirS . dirW
+
+getTile w (Pos x y) = let
+	tiles = worldTiles w
+	(Size dx dy) = worldSize w
+	in if x < 0 || y < 0 || x >= dx || y >= dy then bound else tiles !! y !! x
+
+getAdjacentTiles w pos = map (\f -> getTile w $ f pos) [dirN, dirNE, dirE, dirSE, dirS, dirSW, dirW, dirNW]
+smoothTile' t a = if (length . (filter (wall ==)) $ t:a) >= 5 then wall else floor
+smoothTile w p = smoothTile' (getTile w p) (getAdjacentTiles w p)
+smoothMap w = let
+	tiles = worldTiles w
+	(Size cx cy) = worldSize w
+	in World (Size cx cy) [[smoothTile w $ Pos x y
+	     | x <- [0..(cx-1)]]
+		 | y <- [0..(cy-1)]]
 
 drawTiles (row:rows) y = do
 	C.mvWAddStr C.stdScr y 0 row
@@ -51,7 +74,7 @@ drawTiles [] _ = do return()
 mapSection tiles x y dx dy = take dy $ map (take dx) tiles
 
 drawMap (Game (Size cx cy) (World size tiles) _ _) = let
-	glyphs = map (map glyph) (mapSection tiles 0 0 cx (cy-1))
+	glyphs = reverse $ map (map glyph) (mapSection tiles 0 0 cx (cy-1))
 	in do drawTiles glyphs 0
 
 worldRandoms w n ls = (w {gen = g'}, l) where (l, g') = nRandomLs n ls (gen w)
@@ -76,7 +99,7 @@ drawUI (Game (Size sizeX sizeY) world Lose _) = do
 
 drawUI (Game (Size sizeX sizeY) world Play g) = do
 	drawMap (Game (Size sizeX sizeY) world Play g)
-	C.mvWAddStr C.stdScr (sizeY-1) 0 "Press enter to win, anything else to lose."
+	C.mvWAddStr C.stdScr (sizeY-1) 0 "Press enter to win, s to smooth the map or anything else to lose."
 
 
 updateUI game = do
@@ -85,6 +108,7 @@ updateUI game = do
 
 updateGame (Game size w Start g) _                   = Game size w Play g
 updateGame (Game size w Play g)  (C.KeyChar '\ESC')  = Game size w Lose g
+updateGame (Game size w Play g)  (C.KeyChar 's')     = Game size (smoothMap w) Play g
 updateGame (Game size w Play g)  _                   = Game size w Win g
 updateGame (Game size w Win g)   (C.KeyChar '\ESC')  = Game size w Quit g
 updateGame (Game size w Win g)   _                   = newGame size (worldSize w) g
